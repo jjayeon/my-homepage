a3f8250bc9aaff2b6250d70c9704ec0ed13ef2e7
jjayeon <autumn.chiu.136@gmail.com>
2021-03-18 14:02:23 -0400

    formatting

--- src/App.js
+++ src/App.js
@@ -9,84 +9,97 @@ import bulletimgurl from "./assets/bullet hell js.png";
 import uncrossimgurl from "./assets/uncross the lines.png";
 
 function App() {
-  return (
-    <div className="App">
-      <h1>Projects by Autumn</h1>
-      <p>
-        Hello! This is a website where I keep links to all my most interesting
-        projects. As you can see, my eye for design is underdeveloped, but if
-        you take a dive into my source code, you&apos;ll find that my code logic
-        and organization is much better.
-      </p>
-      <a
-        href="https://github.com/jjayeon/my-homepage"
-        target="_blank"
-        rel="noreferrer noopener"
-      >
-        See source on Github
-      </a>
-      <div className="Cards">
-        <Card
-          title="Bullet Hell JS"
-          copy="A bullet hell shooter written in pure Javascript."
-          img={bulletimgurl}
-          liveurl="https://bullet-hell-js.netlify.app/"
-          giturl="https://github.com/jjayeon/bullet-hell-js"
-        />
-        <Card
-          title="Uncross the Lines"
-          copy='A unique implementation of the "uncross the lines" puzzle game, where you can group and move around multiple dots at once.'
-          img={uncrossimgurl}
-          liveurl="https://uncrossthelines.netlify.app/"
-          giturl="https://github.com/jjayeon/uncross-the-lines"
-        />
-        <Card
-          title="Input.js"
-          copy="A simple Javascript library for handling mouse and keyboard input in a granular, user-agnostic way."
-          giturl="https://github.com/jjayeon/inputjs"
-        />
-        <Card
-          title="Static Site Generator"
-          copy="A simple static site generator, good for rendering plain text with simple CSS backgrounds."
-          giturl="https://github.com/jjayeon/static-site-generator"
-        />
-      </div>
-    </div>
-  );
+    return (
+        <div className="App">
+            <h1>Projects by Autumn</h1>
+            <p>
+                Hello! This is a website where I keep links to all my most
+                interesting projects. As you can see, my eye for design is
+                underdeveloped, but if you take a dive into my source code,
+                you&apos;ll find that my code logic and organization is much
+                better.
+            </p>
+            <a
+                href="https://github.com/jjayeon/my-homepage"
+                target="_blank"
+                rel="noreferrer noopener"
+            >
+                See source on Github
+            </a>
+            <div className="Cards">
+                <Card
+                    title="Bullet Hell JS"
+                    copy="A bullet hell shooter written in pure Javascript."
+                    img={bulletimgurl}
+                    liveurl="https://bullet-hell-js.netlify.app/"
+                    giturl="https://github.com/jjayeon/bullet-hell-js"
+                />
+                <Card
+                    title="Uncross the Lines"
+                    copy='A unique implementation of the "uncross the lines" puzzle game, where you can group and move around multiple dots at once.'
+                    img={uncrossimgurl}
+                    liveurl="https://uncrossthelines.netlify.app/"
+                    giturl="https://github.com/jjayeon/uncross-the-lines"
+                />
+                <Card
+                    title="Input.js"
+                    copy="A simple Javascript library for handling mouse and keyboard input in a granular, user-agnostic way."
+                    giturl="https://github.com/jjayeon/inputjs"
+                />
+                <Card
+                    title="Static Site Generator"
+                    copy="A simple static site generator, good for rendering plain text with simple CSS backgrounds."
+                    giturl="https://github.com/jjayeon/static-site-generator"
+                />
+            </div>
+        </div>
+    );
 }
 
 function Card(props) {
-  return (
-    <div className="Card">
-      <h2>{props.title}</h2>
-      {props.img && (
-        <a href={props.liveurl} target="_blank" rel="noreferrer noopener">
-          <img src={props.img} alt="imagine picture here" />
-        </a>
-      )}
-      <p>
-        {props.liveurl && (
-          <a href={props.liveurl} target="_blank" rel="noreferrer noopener">
-            Try it out
-          </a>
-        )}
-        {props.liveurl && " | "}
-        {props.giturl && (
-          <a href={props.giturl} target="_blank" rel="noreferrer noopener">
-            See source on Github
-          </a>
-        )}
-      </p>
-      <p>{props.copy}</p>
-    </div>
-  );
+    return (
+        <div className="Card">
+            <h2>{props.title}</h2>
+            {props.img && (
+                <a
+                    href={props.liveurl}
+                    target="_blank"
+                    rel="noreferrer noopener"
+                >
+                    <img src={props.img} alt="imagine picture here" />
+                </a>
+            )}
+            <p>
+                {props.liveurl && (
+                    <a
+                        href={props.liveurl}
+                        target="_blank"
+                        rel="noreferrer noopener"
+                    >
+                        Try it out
+                    </a>
+                )}
+                {props.liveurl && " | "}
+                {props.giturl && (
+                    <a
+                        href={props.giturl}
+                        target="_blank"
+                        rel="noreferrer noopener"
+                    >
+                        See source on Github
+                    </a>
+                )}
+            </p>
+            <p>{props.copy}</p>
+        </div>
+    );
 }
 Card.propTypes = {
-  title: PropTypes.string,
-  copy: PropTypes.string,
-  img: PropTypes.string,
-  liveurl: PropTypes.string,
-  giturl: PropTypes.string,
+    title: PropTypes.string,
+    copy: PropTypes.string,
+    img: PropTypes.string,
+    liveurl: PropTypes.string,
+    giturl: PropTypes.string,
 };
 
 export default App;
