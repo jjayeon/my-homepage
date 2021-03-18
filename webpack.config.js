1c22fad3daa093359794871fd254dd9a3bffd7b3
jjayeon <autumn.chiu.136@gmail.com>
2021-03-18 13:12:27 -0400

    cleanup

--- webpack.config.js
+++ webpack.config.js
@@ -6,43 +6,43 @@ const { CleanWebpackPlugin } = require("clean-webpack-plugin");
 const PrettierWebpackPlugin = require("prettier-webpack-plugin");
 
 module.exports = {
-  devtool: "source-map",
-  entry: "./src/index.js",
-  module: {
-    rules: [
-      {
-        enforce: "pre",
-        test: /\.js$/,
-        exclude: /node_modules/,
-        loader: "eslint-loader",
-      },
-      {
-        test: /\.js$/,
-        exclude: /node_modules/,
-        loader: "babel-loader",
-      },
-      {
-        test: /\.css$/,
-        loader: ["style-loader", "css-loader"],
-      },
-      {
-        test: /\.png$/,
-        loader: "file-loader",
-      },
+    devtool: "source-map",
+    entry: "./src/index.js",
+    module: {
+        rules: [
+            {
+                enforce: "pre",
+                test: /\.js$/,
+                exclude: /node_modules/,
+                loader: "eslint-loader",
+            },
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                loader: "babel-loader",
+            },
+            {
+                test: /\.css$/,
+                loader: ["style-loader", "css-loader"],
+            },
+            {
+                test: /\.(jpg|jpeg|png|gif|svg)$/,
+                loader: "file-loader",
+            },
+        ],
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: "./src/index.html",
+        }),
+        new CleanWebpackPlugin(),
+        new PrettierWebpackPlugin(),
     ],
-  },
-  plugins: [
-    new HtmlWebpackPlugin({
-      template: "./src/index.html",
-    }),
-    new CleanWebpackPlugin(),
-    new PrettierWebpackPlugin(),
-  ],
-  devServer: {
-    contentBase: "./dist",
-  },
-  output: {
-    filename: "main.js",
-    path: path.resolve(__dirname, "dist"),
-  },
+    devServer: {
+        contentBase: "./dist",
+    },
+    output: {
+        filename: "main.js",
+        path: path.resolve(__dirname, "dist"),
+    },
 };
